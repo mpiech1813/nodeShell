03790892bbb44be416bfe56ad14d7f1aa871807f
Maciej Pie <maciejpiech18@gmail.com>
2021-01-28 21:00:37 -0600

    pwd function moved to separate file

--- bash.js
+++ bash.js
@@ -1,3 +1,4 @@
+const pwd = require('./pwd');
 // Output a prompt
 process.stdout.write('prompt > ');
 
@@ -5,8 +6,10 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim(); // remove the newline
 
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
-
-  process.stdout.write(process.cwd());
+  if (cmd === 'pwd') {
+    pwd();
+  } else {
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
+  }
 });
